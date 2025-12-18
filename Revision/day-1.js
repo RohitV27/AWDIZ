fe9f1a695539b9611f0b097fc24cc3dc0c27f39f
Rohit <verma.rohit2727@gmail.com>
2025-12-18 10:42:14 +0530

    Create day-1.js

---
+++ Revision/day-1.js
@@ -0,0 +1,11 @@
+// console.log("Awidz")
+// console.log(typeof (15616))
+// console.log(typeof ("awdiz"))
+// console.log(typeof ('65164'))
+// console.log(2 + 2)
+
+    // function add(num1, num2) 
+    // {
+    //     return num1 + num2
+    // }
+    // add (5, 10)
