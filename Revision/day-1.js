c714a722fc9875b9cdd9c44c3ab42e0a88f35601
Rohit <verma.rohit2727@gmail.com>
2025-12-18 21:31:35 +0530

    Update day-1.js

--- Revision/day-1.js
+++ Revision/day-1.js
@@ -1,13 +1,55 @@
-// console.log("Awidz")
-// console.log(typeof (15616))
-// console.log(typeof ("awdiz"))
-// console.log(typeof ('65164'))
-// console.log(2 + 2)
+console.log("Awidz")
+console.log(typeof (15616))
+console.log(typeof ("awdiz"))
+console.log(typeof ('65164'))
+console.log(2 + 2)
 
-    // function add(num1, num2) 
-    // {
-    //     return num1 + num2
-    // }
-    // add (5, 10)
+    function add(num1, num2) 
+    {
+        return num1 + num2
+    }
+    add (5, 10)
 
     console.log(40 % 2 == 0)
+
+    var kuchbhi = 20;
+    console.log(kuchbhi)
+
+    var number1 = 10;
+    var number2 = 20;
+    console.log(number1 , number2)
+    console.log(number1 + number2)
+
+    var obj1 = {
+        x : 20,
+        y : "Hello world!",
+        z : function() {
+            return this.x;
+        }
+    }
+
+    var name = "Rohit"
+    var surname = "Verma"
+    console.log(name + surname)
+
+    var userAge = 10
+    if (userAge >=18) {
+        console.log("User can vote")
+    }
+    else {
+        console.log("User cannot vote")
+    }
+
+    var userAge1 = 19
+    if (userAge1 > 19 && userAge1 < 90) {
+        console.log("User is valid for DL")
+    }
+    else if (userAge1 < 19) {
+        console.log("User age is under limit")
+    }
+    else if (userAge1 >= 90) {
+        console.log("User age is over limit")
+    }
+    else {
+        console.log("User is allowed for LL")
+    }
