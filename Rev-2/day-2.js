73f014ee1570490c654486a2c5cafb99119b9042
Rohit <verma.rohit2727@gmail.com>
2025-12-19 10:31:15 +0530

    Update day-2.js

--- Rev-2/day-2.js
+++ Rev-2/day-2.js
@@ -1,3 +1,8 @@
-for (i = 1; i <= 50; i += 10) {
-    console.log(i);
+// for (i = 1; i <= 50; i += 10) {
+//     console.log(i);
+// }
+
+var numbers = [10, 20, 30, 40, 50, 60];
+for (var i = numbers.length - 1; i > 0; i--) {
+    console.log(numbers[i]);
 }
