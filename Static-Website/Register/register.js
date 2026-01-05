e79d0ab889b4ac8b6e4fc53653d2ef35ff3e659e
Rohit <verma.rohit2727@gmail.com>
2026-01-05 19:22:28 +0530

    registration code

--- Static-Website/Register/register.js
+++ Static-Website/Register/register.js
@@ -1,6 +1,20 @@
 function register(event) {
     event.preventDefault();
-    alert("Hello from Register")
+    // alert("Hello from Register")
 
-    console.log("Hii")
+ var name = document.getElementById("name").value;
+ console.log("name: ", name);
+var email = document.getElementById("email").value;
+console.log("email: ", email);
+var password = document.getElementById("password").value;
+console.log("password: ", password);
+
+if (!name || !email || !password) {
+    alert("All fields are required!")
+}
+
+var userData = {name, email, password};
+localStorage.setItem("Users", JSON.stringify(userData));
+alert("Registration Successful!")
+
 }
